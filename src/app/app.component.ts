38a3820757497fc07b0baf4d3fbda0e0a69c7559
pjgaudr <gaudp00@yahoo.com>
2019-09-24 20:50:00 -0400

    Adding new spares

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -60,20 +60,26 @@ export class AppComponent implements OnInit {
     this.regularForwards.push(new Widget('Trevor Grant', 'trevorandsybil@yahoo.ca'));
     this.regularDefensman.push(new Widget('Yanick Gagnon', 'yanick.gagnon@hotmail.com'));
 
+    this.sparePlayers.push(new Widget('Andrew Allen', 'allenandrew1400@gmail.com'));
     this.sparePlayers.push(new Widget('Benoit Roy', 'benoitroy.x1@gmail.com'));
     this.sparePlayers.push(new Widget('Brendan Arciszewski', 'brendan.arciszewski@edu.uwaterloo.ca'));
     this.sparePlayers.push(new Widget('Bruno Lanthier', 'brunolanthier@gmail.com'));
     this.sparePlayers.push(new Widget('David Gervais', 'dgervais18@hotmail.com'));
     this.sparePlayers.push(new Widget('Dylan Waugh', 'dylwaugh@gmail.com'));
     this.sparePlayers.push(new Widget('Erik Plumadore', 'ebplumadore@gmail.com'));
+    this.sparePlayers.push(new Widget('Gilles Dagenais', 'lpgd2230@gmail.com'));
     this.sparePlayers.push(new Widget('Jordy Keighan', 'jordy.keighan@gmail.com'));
     this.sparePlayers.push(new Widget('Luc Orsali', 'stapounch@gmail.com'));
     this.sparePlayers.push(new Widget('Marc St-Cyr', 'marc.stcyr1@gmail.com'));
     this.sparePlayers.push(new Widget('Mark Wilson', 'mjd.wilson@gmail.com'));
     this.sparePlayers.push(new Widget('Matthew Bowen', 'bowen.matthew@gmail.com'));
+    this.sparePlayers.push(new Widget('Michael Russell', 'michael.l.russell@gmail.com'));
     this.sparePlayers.push(new Widget('Nicholas Parnell', 'nicholas.parnell@gmail.com'));
+    this.sparePlayers.push(new Widget('Nicolas Trudeau', 'nicolas.trudeau@hotmail.com'));
     this.sparePlayers.push(new Widget('Paul Adams-Robenhymer', 'p.robenhymer@gmail.com'));
+    this.sparePlayers.push(new Widget('Pierre Desbiens', 'pierredesbiens11@gmail.com'));
     this.sparePlayers.push(new Widget('Sean Drennan', 'sdrennan@hotmail.com'));
+    this.sparePlayers.push(new Widget('Simon Llewellyn', 'simon.llewellyn@live.com'));
     this.sparePlayers.push(new Widget('Stephen Breault', 'stephen_breault@hotmail.com'));
     this.sparePlayers.push(new Widget('Vincent Hardy', 'hardy.vincent@gmail.com'));
 
